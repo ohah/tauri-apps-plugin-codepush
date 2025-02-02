1eec1c223656d3db604fc76bf27dd33918495323
ohah <bookyoon173@gmail.com>
2025-02-03 05:02:13 +0900

    feat: 데스크탑 코드 추가

--- packages/codepush/src/lib.rs
+++ packages/codepush/src/lib.rs
@@ -1,3 +1,6 @@
+use std::fmt::Debug;
+
+use serde::Deserialize;
 use tauri::{
     plugin::{Builder, TauriPlugin},
     Manager, Runtime,
@@ -32,15 +35,51 @@ impl<R: Runtime, T: Manager<R>> crate::CodepushExt<R> for T {
     }
 }
 
-/// Initializes the plugin.
-pub fn init<R: Runtime>() -> TauriPlugin<R> {
-    Builder::new("codepush")
+#[derive(Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct S3Config {
+    pub bucket: String,
+    pub region: String,
+    pub access_key: String,
+    pub secret_access_key: String,
+}
+
+impl Default for S3Config {
+    fn default() -> Self {
+        S3Config {
+            bucket: String::from("default_bucket"),
+            region: String::from("default_region"),
+            access_key: String::from("default_access_key"),
+            secret_access_key: String::from("default_secret_key"),
+        }
+    }
+}
+// Define the plugin config
+#[derive(Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct Config {
+    pub aws: S3Config,
+    pub download_url: String,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            aws: S3Config::default(), // S3Config도 Default를 구현해야 합니다.
+            download_url: String::from("default_url"),
+        }
+    }
+}
+
+pub fn init<R: Runtime>() -> TauriPlugin<R, Option<Config>> {
+    Builder::<R, Option<Config>>::new("codepush")
         .invoke_handler(tauri::generate_handler![commands::ping])
         .setup(|app, api| {
             #[cfg(mobile)]
             let codepush = mobile::init(app, api)?;
             #[cfg(desktop)]
             let codepush = desktop::init(app, api)?;
+
             app.manage(codepush);
             println!("Codepush plugin initialized");
             Ok(())
